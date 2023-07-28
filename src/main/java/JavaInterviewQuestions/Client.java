b7472b909f86bef3a1fc9f2cc523071b658aa1d0
shrikrishnan <b.shrikrishnan@gmail.com>
2023-07-28 16:47:18 +0530

    java /interviw question

--- src/main/java/JavaInterviewQuestions/Client.java
+++ src/main/java/JavaInterviewQuestions/Client.java
@@ -3,5 +3,20 @@ package JavaInterviewQuestions;
 public class Client {
     public static void main(String[] args) {
 
+        String s1 = " Hello ";
+        String s2 = " Hello ";
+        String s3 = new String("Hello");
+        String s4 = new String("Hello");
+
+        
+
+        System.out.println(s1==s3);
+        System.out.println(System.identityHashCode(s1));
+        System.out.println(System.identityHashCode(s2));
+        System.out.println(System.identityHashCode(s3));
+        System.out.println(System.identityHashCode(s4));
+
+
+
     }
 }
